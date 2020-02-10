a7f40a5808ce024d6ddd8c0d1e2d61b24e0a09f5
SNABITH REDDY SAMA <samasnabith@gmail.com>
2020-02-10 06:27:42 +0530

    Connecting with MongoDB using djongo engine

--- Colconn/settings.py
+++ Colconn/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'Colconn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': '',
+        'HOST': '',
+        'USER':'',
+        'PASSWORD':''
     }
 }
 
